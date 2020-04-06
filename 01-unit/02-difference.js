dfacd987fa4d2ddc004bfe38017ae6606c96e48a
khush1611 <khushboo.umrigar11@gmail.com>
2020-04-06 13:29:18 -0400

    subtraction testing formed for all four cases

--- 01-unit/02-difference.js
+++ 01-unit/02-difference.js
@@ -1,8 +1,10 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function difference() {
-
+function difference(num1, num2) {
+  if(num1 === undefined) { num1=0; }
+  if(num2 === undefined) { num2=0; }
+  return num1 - num2;
 }
 
 // ==================================================
@@ -13,23 +15,28 @@ try {
   // Test Case 1
   // --------------------------------------------------
   // It should return the difference of two numbers.
-  var result = difference(1, 1);
-  if (result !== 0) throw new Error('Expected difference(1, 1) to be 0. Received: ' + result);
+    var result = difference(1, 1);
+    if (result !== 0) throw new Error('Expected difference(1, 1) to be 0. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 2
   // --------------------------------------------------
   // It should ignore additional numbers.
+      var result = difference(3, 1, 1, 1, 1, 1, 1);
+      if (result !== 2) throw new Error('Expected difference(3, 1, 1, 1, 1, 1, 1) to be 2. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
-
+      var result = difference(1);
+      if (result !== 1) throw new Error('Expected difference(1) to be 1. Received: ' + result);
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+      var result = difference();
+      if (result !== 0) throw new Error('Expected difference() to be 0. Received: ' + result);
 
   console.log('All tests passed successfully.');
 
