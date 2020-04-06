152e644fdd93b5e9163c07e1344b343d5be57219
khush1611 <khushboo.umrigar11@gmail.com>
2020-04-06 14:07:49 -0400

    integratio test passed for exponent.

--- 01-unit/04-quotient.js
+++ 01-unit/04-quotient.js
@@ -1,61 +1,60 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function quotient(num1, num2) {
-  if(num1 === undefined) { num1 = 0; }
-  if(num2 === undefined) { num2 = 1; }
+function sum(num1, num2) {
+  if(num1 === undefined) { num1=0; }
+  if(num2 === undefined) { num2=0; }
+  return num1 + num2;
+}
 
- return num1 /  num2;
+function calc(Operation, num1, num2) {
+  switch(Operation)
+  {
+    case 'add': return sum(num1, num2);
+
+    default:  return 'operation not supported';
+  }
+
 }
 
 // ==================================================
 // DEFINE AND EXECUTE TEST CASES
 // ==================================================
 try {
-// --------------------------------------------------
-// Test Case 1
-// --------------------------------------------------
-// It should return the quotient of two numbers.
-  var result = quotient(2, 2);
-  if (result !== 1) throw new Error('Expected quotient(2, 2) to be 1. Received: ' + result);
+  // --------------------------------------------------
+  // Test Case 1
+  // --------------------------------------------------
+  // It should return the correct sum when the user provides: 'add', 1, 1.
+  var result = calc('add', 1, 1);
+  if (result !== 2) throw new Error('Expected calc("add", 1, 1) to be 2. Received: ' + result);
 
-// --------------------------------------------------
-// Test Case 2
-// --------------------------------------------------
-// It should return the text 'ERROR' when the second number is 0.
-  var result = quotient(2, 0);
-  if (result == 0) throw new Error('Expected quotient(2, 0) to be any number execpt for 0. Received: ' + result);
+  // --------------------------------------------------
+  // Test Case 2
+  // --------------------------------------------------
+  // It should return the correct difference when the user provides: 'subtract', 20, 10.
 
-// --------------------------------------------------
-// Test Case 3
-// --------------------------------------------------
-// It should ignore additional numbers.
-  var result = quotient(2, 1, 2, 2, 2, 2, 2);
-  if (result !== 2) throw new Error('Expected quotient(2, 1, 2, 2, 2, 2, 2) to be 2. Received: ' + result);
+  // --------------------------------------------------
+  // Test Case 3
+  // --------------------------------------------------
+  // It should return the correct product when the user provides: 'multiply', 9, 9.
 
-// --------------------------------------------------
-// Test Case 4
-// --------------------------------------------------
-// When invoked with only 1 number, it should return that number.
-  var result = quotient(1);
-  if (result !== 1) throw new Error('Expected quotient(1) to be 1. Received: ' + result);
+  // --------------------------------------------------
+  // Test Case 4
+  // --------------------------------------------------
+  // It should return the correct quotient when the user provides: 'divide', 9, 3.
 
-
-// --------------------------------------------------
-// Test Case 5
-// --------------------------------------------------
-// When invoked with no numbers, it should return 0.
-    var result = quotient();
-    if (result !== 0) throw new Error('Expected quotient() to be 0. Received: ' + result);
-
-console.log('All tests passed successfully.');
-  
-
+  // --------------------------------------------------
+  // Test Case 5
+  // --------------------------------------------------
+  // It should return the message 'Operation not supported.' when the user provides: exponent, 2, 8.
+  var result = calc('exponent', 2, 8);
+  if (result !== 'operation not supported') throw new Error('Expected calc("exponent", 2, 8) to be operation not supported. Received: ' + result);
+  console.log('All tests passed successfully.');
 
 // ==================================================
 // PRINT ERRORS
 // ==================================================
 } catch (e) {
-console.warn('Whoops, the following test did not pass:');
-console.error(e.message);
+  console.warn('Whoops, the following test did not pass:');
+  console.error(e.message);
 }
